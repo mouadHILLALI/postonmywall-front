b878c06a3300794f71587d0c5da83023f5094767
mouadHILLALI <mauadtoto001@gmail.com>
2026-04-26 21:54:14 +0100

    changed the backend api

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  production: false,
-  apiUrl: 'http://localhost:8080/api/v1',
+  production: true,
+  apiUrl: 'https://postonmywall-tech-test-production.up.railway.app/api/v1',
 };
